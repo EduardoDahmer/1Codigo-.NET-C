482f22c58be87222b3b737618adadfcece502cf9
EduardoDahmer <eduardofdahmer@hotmail.com>
2022-01-19 13:04:06 -0300

    Alteração no código

--- Program.cs
+++ Program.cs
@@ -4,15 +4,19 @@
  {
         static void Main(string[] args) {
             //continue a solucao com a insercao da variavel corretamente
+            Console.WriteLine("Digite dois números para descobrir se eles são múltiplos!");
+            Console.WriteLine("");
+            Console.WriteLine("Digite o primeiro número: ");
             int A;
             int B;
             A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Agora, digite o segundo número: ");
             B = int.Parse(Console.ReadLine());
 
             if (A % B  == 0 || B % A == 0) {
-                Console.WriteLine("Sao Multiplos");
+                Console.WriteLine(" Os números digitados são múltiplos");
             }else{
-                Console.WriteLine("Nao sao Multiplos");
+                Console.WriteLine(" Os números digitados não são múltiplos");
             }
             
         }
