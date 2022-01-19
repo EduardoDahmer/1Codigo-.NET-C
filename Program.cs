43a54cea080069770e132a59e30aff3fdf66d345
EduardoDahmer <eduardofdahmer@hotmail.com>
2022-01-19 12:59:48 -0300

    Commit com código

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+ class minhaClasse 
+ {
+        static void Main(string[] args) {
+            //continue a solucao com a insercao da variavel corretamente
+            int A;
+            int B;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            if (A % B  == 0 || B % A == 0) {
+                Console.WriteLine("Sao Multiplos");
+            }else{
+                Console.WriteLine("Nao sao Multiplos");
+            }
+            
+        }
+    }
